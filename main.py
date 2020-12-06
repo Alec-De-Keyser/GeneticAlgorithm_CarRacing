0b72f64494376900c581a8e4fd7ffda84c4e6924
Alec De Keyser <alec.dekeyser@live.be>
2020-12-06 11:52:10 +0100

    commit1

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ import numpy as np
 import gym.envs.box2d.car_racing
 
 
-# Press the green button in the gutter to run the script.
+def get_output(obs, net):
+    print(obs)
+    return obs
+
+
 if __name__ == '__main__':
 
     ga = GeneticAlgorithm(10, 96*96*3, 50, 3)
     ga.init_population()
     env = gym.make('CarRacing-v0')
+    reward = float(0)
+    rewards = []
+    observation = env.reset()
+    done = False
+    for i in ga.nets:
+        x = get_output(observation, ga.nets[i])
+#    while not done:
+
